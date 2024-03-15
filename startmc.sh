9209104d734a7d82c983e1c0e8680c5705d74c60
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2024-03-15 16:26:32 +0100

    tyj

--- startmc.sh
+++ startmc.sh
@@ -1,2 +1,10 @@
 cd ./server
+rm ./plugins/Essentials/userdata/*
+rm ./plugins/Essentials/warps/*
+rsync -vau --remove-source-files ../data/userdata/* ./plugins/Essentials/userdata/
+rsync -vau --remove-source-files ../data/warps/* ./plugins/Essentials/warps/
 java -Djline.terminal=jline.UnsupportedTerminal -Xmx512M -jar server.jar nogui
+rm ../data/userdata/*
+rm ../data/warps/*
+rsync -vau --remove-source-files ./plugins/Essentials/userdata/* ../data/userdata/
+rsync -vau --remove-source-files ./plugins/Essentials/warps/* ../data/warps/
